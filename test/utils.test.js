beb9d54a48329a28666c3d135883cd0f51651242
Ryan Flitcroft <ryanflitcroft@ymail.com>
2021-12-01 16:12:00 -0800

    forgot to tdd, test for utils.js is now passing

--- test/utils.test.js
+++ test/utils.test.js
@@ -6,7 +6,7 @@ const test = QUnit.test;
 test('should return a string describing the current state', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `You clicked on the head 4 times, on the middle 5 times, and on the bottom 1 time. And everybody loves your character's catchphrases:`;
+    const expected = `Your head has been changed: ${4} times!!\nYour middle has been changed: ${5} times!!\nYour bottom has been changed: ${1} times!!`;
     
     //Act 
     // Call the function you're testing and set the result to a const
@@ -14,5 +14,5 @@ test('should return a string describing the current state', (expect) => {
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual, expected, 'this test proves that this function will return a string concat with the 3 arguments passed to the function');
 });
